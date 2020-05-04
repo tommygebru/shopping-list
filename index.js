1f934e04ec408c3cbd15a8fb42a67068f61bcc5b
tommygebru <gebru.t@gmail.com>
2020-05-04 08:20:30 -0700

    add comments to js

---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
+
+check and uncheck items on the list by clicking the "Check" button
+
+permanently remove items from the list
+*/
