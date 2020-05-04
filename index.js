fb2c0e63ae9397d305ad42e058a4eea67dd1526f
tommygebru <gebru.t@gmail.com>
2020-05-04 09:41:47 -0700

    add list item

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
-/*
-enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
+$(document).ready(function(){
+  /*
+-  enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
+-  check and uncheck items on the list by clicking the "Check" button
+-  permanently remove items from the list
 
-check and uncheck items on the list by clicking the "Check" button
+  submit(), preventDefault(), toggleClass(), closest()
+  find(), empty(), attr({,}), val(), parseInt(), append()
+  */
 
-permanently remove items from the list
-*/
+let addItemForm = document.getElementById("js-shopping-list-form");
+let addItemButton = addItemForm.getElementsByTagName("button")[0];
+let itemEntry = document.getElementById("shopping-list-entry");
+let shoppingList = document.getElementsByClassName("shopping-list")[0];
+let li = document.createElement("LI");
+li.innerHTML = `
+      <span class="shopping-item"></span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    <!-- L I S T   I T E M -->`
+
+
+//create element
+
+function handleAddItem(){
+  event.preventDefault();
+console.log(itemEntry.value);
+shoppingList.appendChild(li); //works
+
+//shoppingList.innerHTML = stringList;
+//append element
+}//function
+addItemButton.addEventListener("click", handleAddItem);
+
+
+
+});//ready function
