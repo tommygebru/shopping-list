f5f942d2a0dd39a3b4ac3d08ab7a78a09297a1de
tommygebru <gebru.t@gmail.com>
2020-05-05 13:56:57 -0700

    delete shopping item from list

--- index.js
+++ index.js
@@ -1,42 +1,30 @@
-$(document).ready(function() {
-	/*
--  enter items they need to purchase by entering text and hitting "Return" or clicking the "Add item" button
--  check and uncheck items on the list by clicking the "Check" button
--  permanently remove items from the list
+  $("#js-shopping-list-form > button").on("click", function(event) {
+    event.preventDefault();
+    if($("#shopping-list-entry").val()){
+      $(".shopping-list").append(`
+      <li>
+        <span class="shopping-item">${$("#shopping-list-entry").val()}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>
+      <!-- L I S T   I T E M -->`
+      );
+    }
+  });//function
 
-  submit(), preventDefault(), toggleClass(), closest()
-  find(), empty(), attr({,}), val(), parseInt(), append()
-  */
-	let addItemForm = document.getElementById("js-shopping-list-form");
-	let addItemButton = addItemForm.getElementsByTagName("button")[0];
-	let itemEntry = document.getElementById("shopping-list-entry");
-	let shoppingList = document.getElementsByClassName("shopping-list")[0];
-	let li = document.createElement("LI");
-	li.innerHTML = `
-      <span class="shopping-item"></span>
-      <div class="shopping-item-controls">
-        <button class="shopping-item-toggle">
-          <span class="button-label">check</span>
-        </button>
-        <button class="shopping-item-delete">
-          <span class="button-label">delete</span>
-        </button>
-      </div>
-    <!-- L I S T   I T E M -->`;
-	//create element
-	function handleAddItem() {
-		event.preventDefault();
-		//add LI and components
-		shoppingList.appendChild(li);
-		//add shopping-item
-		let listIndex = (document.getElementsByClassName("shopping-item").length) - 1;
-		document.getElementsByClassName("shopping-item")[listIndex].innerHTML = itemEntry.value;
-	} //function
-	addItemButton.addEventListener("click", handleAddItem);
-	//written in jquery, strikethrough shopping list entries
-	$(".button-label").on("click", function(event) {
-		event.preventDefault();
-		let itemEntry = $(event.target).parent().parent().parent().find(".shopping-item");
-		itemEntry.toggleClass("shopping-item__checked");
+
+
+	$(".shopping-list").on("click", ".shopping-item-toggle", function(event) {
+		$(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
 	}); //function
-}); //ready function
+
+
+  $(".shopping-list").on("click", ".shopping-item-delete", function(event){
+   $(event.target).closest("li").remove();
+ });//function
